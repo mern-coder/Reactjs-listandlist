6683622c0a143cb02a000c6abb8b9e2304527437
Manoj <manojd@rgmobiles.com>
2020-10-02 12:55:35 +0530

    listandkey

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+//Array objact
+// unique id array
+  const Courses=[
+    {id:1,name:'reactjs'},
+    {id:2,name:'listandkey'},
+    {id:3,name:'classjs'},
+    {id:4,name:'function'},
+    {id:5,name:'lifcycle'},
+    {id:6,name:'mern'},
+    {id:7,name:'nodejs'},
+    {id:8,name:'expreessjs'},
+    {id:9,name:'es6'},
+    {id:10,name:'render'},
+    {id:11,name:'pros,state'}
+    
+  ];
+  //without  unique id array
+
+  const ReactCons=[
+    {name:'reactjs'},
+    {name:'listandkey'},
+    {name:'classjs'},
+    {name:'function'},
+    {name:'lifcycle'},
+    {name:'mern'},
+  ];
+
+  //nested array
+  const Developer=[
+{
+  name:'mark',
+  skills:[
+    {name:'fb',type:'fontend'},
+    {name:'expressjs',type:'Baclend'}
+  ]
+},
+{
+  name:'manoj',
+  skills:[
+    {name:'reactjs',type:'fontend'},
+    {name:'nodejs',type:'Baclend'}
+  ]
+},
+  ];
+
+  // object
+  const person={
+    name:'mark',
+    email:'m2@gmail.com',
+    website:'fb.con'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ textAlign: "center" }}>React List and keys....!</h1>
+      {/* <ul>{
+       Courses.map(data=>(
+       <li>{data.id}</li>
+       ))
+       }
+       </ul> */}
+      {/* unique id in array ----key=data.id */}
+      {/* //arrayObject */}
+      <h1>Reactjs list and keys...!</h1>
+      <ul>
+        {Courses.map((data) => (
+          <li key={data.id}>{data.name}</li>
+        ))}
+      </ul>
+      {/* without unique id in array key=index*/}
+      <ul>
+        {ReactCons.map((data, index) => (
+          <li key={index}>{data.name}</li>
+        ))}
+      </ul>
+      <div>
+        <h1>ArrayObject map...!</h1>
+        {Developer.map((dev, index) => (
+          <div key={index}>
+            <h2>{dev.name}skills</h2>
+            <div>
+              {dev.skills.map((skill, index) => (
+                <p key={index}>
+                  {skill.type}-{skill.name}
+                </p>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* object */}
+    <div>
+      <h1>Object map.....!</h1>
+      {Object.keys(person).map(key=>(
+        <p>{person[key]}</p>
+
+      ))}
     </div>
+    </div>
+
+   
   );
 }
 
